e9763265713b57e384b7a2aa61ef1a9e9d5ccf2c
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-24 12:11:14 -0400

    Update

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+document.write('<div class="container" style="display: flex; justify-content: center; align-content: center;">\
+    <a href="https://twitter.com/Pirates5992" target="_blank"><i class="fa fa-twitter fa-lg social-footer" aria-hidden="true"></i></a>\
+    <a href="https://www.instagram.com/pirates5992/" target="_blank"><i class="fa fa-instagram fa-lg social-footer" aria-hideen="true"></i></a>\
+    <a href="https://youtube.com" target="_blank"><i class="fa fa-youtube-play fa-lg social-footer" aria-hidden="true"></i></a>\
+    <a href="mailto:setonhallrobotics@gmail.com"><i class="fa fa-envelope-o fa-lg social-footer" aria-hidden="true"></i></a>\
+    <a href="https://www.shp.org/" target="_blank"><i class="fa fa-graduation-cap fa-lg social-footer" aria-hidden="true"></i></a>\
+  </div>\
+  </div>\
+  <div class="footer-copyright">\
+    <div class="container">\
+    <i class="fa fa-copyright"></i> 2018 Dhrumin Shah\
+    </div>\
+  </div>');
--- js/nav.js
+++ js/nav.js
@@ -14,7 +14,7 @@ document.write('<ul class="sidenav" id="mobile-demo">\
 <div class="navbar-fixed" id="myNav">\
     <nav>\
       <div class="nav-wrapper">\
-        <a href="index.html" class="brand-logo"><img src="img/banner2.png" id="pirateFace"></a>\
+        <a href="index.html" class="brand-logo"><img src="img/banner2.png"></a>\
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>\
         <ul class="right hide-on-med-and-down">\
           <li><a href="about.html" class="hvr-underline-from-center">About</a></li>\
@@ -23,11 +23,6 @@ document.write('<ul class="sidenav" id="mobile-demo">\
           <li><a href="mentors.html" class="hvr-underline-from-center">Mentors</a></li>\
           <li><a href="sponsors.html" class="hvr-underline-from-center">Sponsors</a></li>\
           <li><a href="resources.html" class="hvr-underline-from-center">Resources</a></li>\
-          <li><a href="https://twitter.com/Pirates5992" target="_blank" class="hvr-underline-from-center"><img src="img/twitter.png" class="social"></a></li>\
-          <li><a href="https://www.instagram.com/pirates5992/" target="_blank" class="hvr-underline-from-center"><img src="img/instagram.png" class="social"></a></li>\
-          <li><a href="https://youtube.com" target="_blank" class="hvr-underline-from-center"><img src="img/youtube.png" class="social"></a></li>\
-          <li><a href="mailto:setonhallrobotics@gmail.com" class="hvr-underline-from-center"><img src="img/email.png" class="social"></a></li>\
-          <li><a href="https://www.shp.org/" target="_blank" class="hvr-underline-from-center"><img src="img/school.png" class="social"></a></li>\
         </ul>\
       </div>\
     </nav>\

25f846e369f9a658ce2071841142625d4840f0ca
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-24 23:57:02 -0400

    update

--- js/footer.js
+++ js/footer.js
@@ -4,6 +4,7 @@ document.write('<div class="container" style="display: flex; justify-content: ce
     <a href="https://youtube.com" target="_blank"><i class="fa fa-youtube-play fa-lg social-footer" aria-hidden="true"></i></a>\
     <a href="mailto:setonhallrobotics@gmail.com"><i class="fa fa-envelope-o fa-lg social-footer" aria-hidden="true"></i></a>\
     <a href="https://www.shp.org/" target="_blank"><i class="fa fa-graduation-cap fa-lg social-footer" aria-hidden="true"></i></a>\
+    <a id="donate-footer" href="#">Donate</a>\
   </div>\
   </div>\
   <div class="footer-copyright">\

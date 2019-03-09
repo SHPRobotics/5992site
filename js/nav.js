7b273c2248b585feeb8f7257cac77d15d9e17820
Dhrumin-Shah <dhrumin1128@gmail.com>
2019-03-08 22:28:14 -0500

    Update

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,6 @@
 document.getElementById('myNav').innerHTML = '<ul class="sidenav" id="mobile-demo">\
           <li><a href="about.html" class="hvr-underline-from-left">About</a></li>\
           <li><a href="frc.html" class="hvr-underline-from-left">FRC</a></li>\
-          <li><a href="robots.html" class="hvr-underline-from-left">Robots</a></li>\
           <li><a href="mentors.html" class="hvr-underline-from-left">Mentors</a></li>\
           <li><a href="sponsors.html" class="hvr-underline-from-left">Sponsors</a></li>\
           <li><a href="resources.html" class="hvr-underline-from-left">Resources</a></li>\
@@ -9,12 +8,11 @@ document.getElementById('myNav').innerHTML = '<ul class="sidenav" id="mobile-dem
 <div class="navbar-fixed" id="myNav">\
     <nav>\
       <div class="nav-wrapper">\
-        <a href="index.html" class="brand-logo"><img src="img/banner2.png"></a>\
+        <a href="index.html" class="brand-logo"><img src="img/banner.png"></a>\
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>\
         <ul class="right hide-on-med-and-down">\
           <li><a href="about.html" class="hvr-underline-from-center">About</a></li>\
           <li><a href="frc.html" class="hvr-underline-from-center">FRC</a></li>\
-          <li><a href="robots.html" class="hvr-underline-from-center">Robots</a></li>\
           <li><a href="mentors.html" class="hvr-underline-from-center">Mentors</a></li>\
           <li><a href="sponsors.html" class="hvr-underline-from-center">Sponsors</a></li>\
           <li><a href="resources.html" class="hvr-underline-from-center">Resources</a></li>\

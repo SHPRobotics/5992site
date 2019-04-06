7701173668d30cae656f91c3d4c773fa507de50f
Dhrumin-Shah <dhrumin1128@gmail.com>
2019-04-06 10:31:05 -0400

    Update

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 document.getElementById('myNav').innerHTML = '<ul class="sidenav" id="mobile-demo">\
           <li><a href="about.html" class="hvr-underline-from-left">About</a></li>\
           <li><a href="frc.html" class="hvr-underline-from-left">FRC</a></li>\
+          <li><a href="events.html" class="hvr-underline-from-left">Events</a></li>\
           <li><a href="mentors.html" class="hvr-underline-from-left">Mentors</a></li>\
           <li><a href="sponsors.html" class="hvr-underline-from-left">Sponsors</a></li>\
           <li><a href="resources.html" class="hvr-underline-from-left">Resources</a></li>\
@@ -13,6 +14,7 @@ document.getElementById('myNav').innerHTML = '<ul class="sidenav" id="mobile-dem
         <ul class="right hide-on-med-and-down">\
           <li><a href="about.html" class="hvr-underline-from-center">About</a></li>\
           <li><a href="frc.html" class="hvr-underline-from-center">FRC</a></li>\
+          <li><a href="events.html" class="hvr-underline-from-center">Events</a></li>\
           <li><a href="mentors.html" class="hvr-underline-from-center">Mentors</a></li>\
           <li><a href="sponsors.html" class="hvr-underline-from-center">Sponsors</a></li>\
           <li><a href="resources.html" class="hvr-underline-from-center">Resources</a></li>\
